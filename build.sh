cc99a6c68c8134c9f9748f9349d9e8c0765da7de
qians <qiangezaici@outlook.com>
2025-03-17 11:31:46 +0800

    feat: set WEE_ALLOC_STATIC_ARRAY_BACKEND_BYTES 2MiB

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cbindgen --crate riscv-build-test --output riscv_test.h
-cargo build --release --target riscv64gc-unknown-none-elf
-cargo build --release --target arm-unknown-linux-gnueabi
-cargo build --release --target armv7-unknown-linux-gnueabi
+WEE_ALLOC_STATIC_ARRAY_BACKEND_BYTES=2097152 cargo build --release --target riscv64gc-unknown-none-elf
+# WEE_ALLOC_STATIC_ARRAY_BACKEND_BYTES=2097152 cargo build --release --target arm-unknown-linux-gnueabi
+# WEE_ALLOC_STATIC_ARRAY_BACKEND_BYTES=2097152 cargo build --release --target armv7-unknown-linux-gnueabi
